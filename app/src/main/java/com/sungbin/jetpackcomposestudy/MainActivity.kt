c6caa679d798d095e680429534a7f447502e4433
tommy226 <anstkd226@gmail.com>
2023-04-05 15:01:20 +0900

    gradle 빌드 에러 수정

--- app/src/main/java/com/sungbin/jetpackcomposestudy/MainActivity.kt
+++ app/src/main/java/com/sungbin/jetpackcomposestudy/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sungbin.jetpackcomposestudy.ui.theme.JetpackComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,10 +38,20 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(value = "", onValueChange = {},
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp))
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeStudyTheme {
-        Greeting("Android")
+        SearchBar()
     }
 }
