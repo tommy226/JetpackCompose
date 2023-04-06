424a5d771cd31d1bc262482e7c7e92fa0d626e8f
tommy226 <anstkd226@gmail.com>
2023-04-06 14:30:08 +0900

    test

--- app/src/main/java/com/sungbin/jetpackcomposestudy/MainActivity.kt
+++ app/src/main/java/com/sungbin/jetpackcomposestudy/MainActivity.kt
@@ -3,14 +3,22 @@ package com.sungbin.jetpackcomposestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sungbin.jetpackcomposestudy.ui.theme.JetpackComposeStudyTheme
@@ -61,10 +69,65 @@ fun SearchBar(
             .heightIn(min = 56.dp)
     )
 }
+
+@Composable
+fun AlignYourBodyElement(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .size(88.dp)
+                .clip(CircleShape)
+                .clickable {  }
+        )
+        Text(text = "Inversions")
+    }
+}
+
+
+
+@Composable
+fun FavoriteCollectionCard(
+    modifier: Modifier = Modifier
+) {
+    Surface(shape = MaterialTheme.shapes.small, modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(192.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(56.dp)
+            )
+            Text(text = "Nature meditations")
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun FavoriteCollectionCardPreview(){
+    JetpackComposeStudyTheme {
+        FavoriteCollectionCard()
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeStudyTheme {
-        SearchBar()
+        Column(modifier = Modifier.padding(8.dp)) {
+            SearchBar()
+            AlignYourBodyElement(modifier = Modifier.padding(8.dp))
+            FavoriteCollectionCard(modifier = Modifier.padding(8.dp))
+        }
+
     }
 }
